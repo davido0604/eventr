356be1abfb784bcd1de69d45b82cf874f9e294c3
davido0604 <100728190+davido0604@users.noreply.github.com>
2022-04-07 11:27:44 +0200

    david 0704

--- app/app/src/main/java/com/example/eventr/LoginFragment.kt
+++ app/app/src/main/java/com/example/eventr/LoginFragment.kt
@@ -1,6 +1,5 @@
 package com.example.eventr
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,11 +10,10 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
-import androidx.navigation.fragment.findNavController
 
 class LoginFragment : Fragment() {
 
-    lateinit var button1: Button
+    lateinit var loginButton: Button
     lateinit var username: EditText
     lateinit var password: EditText
 
@@ -34,11 +32,13 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        button1 = view.findViewById(R.id.loginbtn)
+
+
+        loginButton = view.findViewById(R.id.loginbtn)
         username = view.findViewById(R.id.username)
         password = view.findViewById(R.id.password)
 
-        button1.setOnClickListener {
+        loginButton.setOnClickListener {
             if (username.text.toString().length == 0) {
                 Toast.makeText(activity, "vennligst skriv inn brukernavn", Toast.LENGTH_SHORT).show();
             } else if (password.text.toString().length == 0) {
--- app/app/src/main/java/com/example/eventr/MainActivity.kt
+++ app/app/src/main/java/com/example/eventr/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.eventr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import androidx.navigation.findNavController
@@ -11,23 +13,31 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+        lateinit var bottomNavigationView: BottomNavigationView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.commit {
-            setReorderingAllowed(true)
-            replace<LoginFragment>(R.id.fragmentContainerView)
-        }
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+
         val navController = findNavController(R.id.fragmentContainerView)
         val appBarConfiguration = AppBarConfiguration(setOf(R.id.searchMainFragment, R.id.profileFragment))
         setupActionBarWithNavController(navController, appBarConfiguration)
         supportActionBar?.hide()
 
-        bottomNavigationView.setupWithNavController(navController)
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace<LoginFragment>(R.id.fragmentContainerView)
+        }
+    }
 
+    fun hideNavBar() {
+        bottomNavigationView.visibility = View.GONE
+    }
 
-
+    fun showNavBar() {
+        bottomNavigationView.visibility = View.VISIBLE
     }
 }
